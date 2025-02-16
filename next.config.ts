2496ceb6732780291691b63e6a6d072cfa1169f0
Thitipong Tapianthong <thitipong.t@email.kmutnb.ac.th>
2025-02-17 00:00:40 +0700

    chore: clean up Next.js configuration by removing unused options

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  distDir: 'docs',
-  images: {
-      unoptimized: true,
-  },
-  basePath: '/time-to-timestamp',
 };
 
 export default nextConfig;
