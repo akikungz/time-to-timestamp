c1ddc5338cbb3faafaf2d8f6b320da937d8add98
Thitipong Tapianthong <thitipong.t@email.kmutnb.ac.th>
2025-03-18 18:39:16 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,30 @@
 "use client";
 import InputTimestamp from "@/components/InputTimestamp";
-import { addHandler, clearHandler, exportHandler, loadHandler, saveHandler } from "@/handler";
+import {
+  addHandler,
+  clearHandler,
+  exportHandler,
+  loadHandler,
+  saveHandler,
+} from "@/handler";
 import type { TimestampInput } from "@/types";
 import { getSeconds } from "@/utils";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 export default function Home() {
   const [inputCount, setInputCount] = useState(0);
   const [inputTemp, setInputTemp] = useState<TimestampInput[]>([]);
 
-  const [startTime, setStartTime] = useState("");
-  const optStartTime = getSeconds(startTime);
+  const [startTime, setStartTime] = useState<string>("");
+  const optStartTime = useMemo(() => getSeconds(startTime), [startTime]);
 
   const [exportText, setExportText] = useState<string | undefined>();
 
   useEffect(() => {
     if (inputTemp.length > 0 && optStartTime !== undefined) {
       setExportText(exportHandler(inputTemp, optStartTime));
-    } 
-  }, [inputTemp, inputCount]);
+    }
+  }, [inputTemp, inputCount, optStartTime]);
 
   return (
     <div className="w-full flex flex-col justify-center items-center">
@@ -27,22 +33,23 @@ export default function Home() {
         <div className="flex flex-col gap-2 justify-center items-start w-full">
           <span>Start time</span>
           <div className="flex flex-row justify-center items-center gap-2 w-full">
-            <input 
-              type="text" 
+            <input
+              type="text"
               className="border border-gray-300 p-2 rounded text-black w-full"
               placeholder="hh:mm:ss"
               value={startTime}
               onChange={(e) => setStartTime(e.target.value)}
             />
-            <span>
-              { optStartTime !== undefined ? "✅" : "❌" }
-            </span>
+            <span>{optStartTime !== undefined ? "✅" : "❌"}</span>
           </div>
         </div>
         <span className="w-full text-start -mb-1 mt-1">Timestamp list</span>
         {[...Array(inputCount)].map((_, index) => (
-          <div className="flex flex-row gap-2 justify-center items-center w-full" key={index}>
-            <InputTimestamp 
+          <div
+            className="flex flex-row gap-2 justify-center items-center w-full"
+            key={index}
+          >
+            <InputTimestamp
               key={index}
               timestampInput={inputTemp[index]}
               setTimestampInput={(timestampInput) => {
@@ -53,7 +60,7 @@ export default function Home() {
                 });
               }}
             />
-            <button 
+            <button
               className="text-white font-bold rounded"
               onClick={() => {
                 setInputCount(inputCount - 1);
@@ -69,15 +76,22 @@ export default function Home() {
           </div>
         ))}
         <div className="flex flex-row justify-center items-center gap-2 w-full">
-          <button 
+          <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
             onClick={() => addHandler(setInputCount, setInputTemp)}
           >
             Add Item
           </button>
-          <button 
+          <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => clearHandler(setInputCount, setInputTemp, setStartTime, setExportText)}
+            onClick={() =>
+              clearHandler(
+                setInputCount,
+                setInputTemp,
+                setStartTime,
+                setExportText,
+              )
+            }
           >
             Clear
           </button>
@@ -91,7 +105,9 @@ export default function Home() {
           </button>
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
-            onClick={() => loadHandler(setInputTemp, setInputCount, setStartTime)}
+            onClick={() =>
+              loadHandler(setInputTemp, setInputCount, setStartTime)
+            }
           >
             Load
           </button>
@@ -108,18 +124,16 @@ export default function Home() {
           </button>
         </div>
 
-        {
-          exportText && (
-            <div className="flex flex-col gap-2 justify-center items-start w-full mt-4">
-              <span>Export Preview</span>
-              <div className="border border-gray-300 p-2 rounded text-white w-full">
-                { exportText?.split("\n").map((line, index) => (
-                  <div key={index}>{line}</div>
-                )) }
-              </div>
+        {exportText && (
+          <div className="flex flex-col gap-2 justify-center items-start w-full mt-4">
+            <span>Export Preview</span>
+            <div className="border border-gray-300 p-2 rounded text-white w-full">
+              {exportText
+                ?.split("\n")
+                .map((line, index) => <div key={index}>{line}</div>)}
             </div>
-          )
-        }
+          </div>
+        )}
       </div>
     </div>
   );
